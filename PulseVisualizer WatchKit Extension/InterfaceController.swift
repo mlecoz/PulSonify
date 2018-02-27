08788999522ecba1678b004ffa66b7d5b6bbee11
Marissa Le Coz <Marissa.C.Le.Coz.17@dartmouth.edu>
2018-02-26 20:59:35 -0500

    cleanup

--- PulseVisualizer WatchKit Extension/InterfaceController.swift
+++ PulseVisualizer WatchKit Extension/InterfaceController.swift
@@ -14,72 +14,11 @@ import HealthKit
 class InterfaceController: WKInterfaceController, HKWorkoutSessionDelegate {
     
     @IBOutlet var bpm: WKInterfaceLabel!
-//    let healthStoreManager = HealthStoreManager()
     let healthStore = HKHealthStore()
     var workoutSession: HKWorkoutSession?
     
     func workoutSession(_ workoutSession: HKWorkoutSession, didChangeTo toState: HKWorkoutSessionState, from fromState: HKWorkoutSessionState, date: Date) {
         print("here")
-        
-        if HKHealthStore.isHealthDataAvailable() {
-            if toState == .running {
-                if let query = createHeartRateStreamingQuery() {
-                    self.healthStore.execute(query)
-                }
-            }
-        }
-        else {
-            print("Healthkit unavailable")
-        }
-        
-        
-        
-//            let heartRate = HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!
-//            let query = HKSampleQuery(sampleType: heartRate, predicate: .none, limit: 0, sortDescriptors: nil) { query, results, error in
-//
-//                guard let count = results?.count else { return }
-//                if count > 0 {
-//                    guard let results = results as? [HKQuantitySample] else { return }
-//                    for result in results {
-//                        let heartRate = result.quantity.doubleValue(for: HKUnit.count().unitDivided(by: HKUnit.minute()))
-//                        self.bpm.setText(String(heartRate))
-//                    }
-//                }
-//            }
-//            self.healthStore.execute(query)
-        
-        
-        
-        
-        
-        
-        
-            
-//            let datePredicate = HKQuery.predicateForSamples(withStart: Date(), end: nil, options: .strictStartDate)
-//            let devicePredicate = HKQuery.predicateForObjects(from: [HKDevice.local()])
-//            let queryPredicate = NSCompoundPredicate(andPredicateWithSubpredicates:[datePredicate, devicePredicate])
-//            let quantityType = HKObjectType.quantityType(forIdentifier: .heartRate)!
-//
-//            let query = HKAnchoredObjectQuery(type: quantityType, predicate: queryPredicate, anchor: nil,
-//                                              limit: HKObjectQueryNoLimit) { (query, samplesOrNil, deletedObjectsOrNil, newAnchor, errorOrNil) in
-//
-//                                                guard let heartSamples = samplesOrNil else { return }
-//                                                for heartSample in heartSamples {
-//                                                    self.bpm.setText(String(heartSample))
-//                                                }
-//            }
-////            query.updateHandler = handler
-//            self.healthStore.execute(query)
-            
-            
-            
-//            startQuery(ofType: typeIdentifier, from: Date()) { _, samples, _, _, error in
-//                guard let quantitySamples = samples as? [HKQuantitySample] else {
-//                    print("Distance walking running query failed with error: \(String(describing: error))")
-//                    return
-//                }
-//                updateHandler(quantitySamples)
-//            }
     }
     
     func workoutSession(_ workoutSession: HKWorkoutSession, didFailWithError error: Error) {
@@ -89,51 +28,9 @@ class InterfaceController: WKInterfaceController, HKWorkoutSessionDelegate {
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         workoutSession?.delegate = self
-//        let workoutConfiguration = HKWorkoutConfiguration()
-//        workoutConfiguration.activityType = .other
-//        workoutConfiguration.locationType = .unknown
-//
-//        do {
-//            try self.workoutSession = HKWorkoutSession(configuration: workoutConfiguration)
-//        }
-//        catch {
-//            fatalError(error.localizedDescription)
-//        }
-        
-//        guard let workoutSession = self.workoutSession else { return }
-//        self.healthStoreManager.start(workoutSession)
-//
-//        let bpmType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)
-        
-        
-//        let sampleType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!
-//
-//        let query = HKObserverQuery(sampleType: sampleType, predicate: nil) { query, completionHandler, error in
-//
-//            if error != nil {
-//
-//                // Perform Proper Error Handling Here...
-//                print("\(error.localizedDescription)")
-//                abort()
-//            }
-//
-//            // Take whatever steps are necessary to update your app's data and UI
-//            // This may involve executing other queries
-////            self.updateDailyStepCount()
-//            self.bpm.setText()
-//
-//            // If you have subscribed for background updates you must call the completion handler here.
-//            // completionHandler()
-//        }
-//
-//        healthStore.executeQuery(query)
-        
-        
-        
-        
     }
     
-    // from https://github.com/coolioxlr/watchOS-2-heartrate/blob/master/VimoHeartRate%20WatchKit%20App%20Extension/InterfaceController.swift
+    // modified from https://github.com/coolioxlr/watchOS-2-heartrate/blob/master/VimoHeartRate%20WatchKit%20App%20Extension/InterfaceController.swift
     func createHeartRateStreamingQuery() -> HKQuery? {
         
         if !HKHealthStore.isHealthDataAvailable() {
@@ -143,21 +40,18 @@ class InterfaceController: WKInterfaceController, HKWorkoutSessionDelegate {
         guard let quantityType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate) else { return nil }
         
         let heartRateQuery = HKAnchoredObjectQuery(type: quantityType, predicate: nil, anchor: nil, limit: Int(HKObjectQueryNoLimit)) { (query, sampleObjects, deletedObjects, newAnchor, error) -> Void in
-//            guard let newAnchor = newAnchor else {return}
-//            self.anchor = newAnchor
             self.updateHeartRate(samples: sampleObjects)
         }
         
         heartRateQuery.updateHandler = {(query, samples, deleteObjects, newAnchor, error) -> Void in
-//            self.anchor = newAnchor!
             self.updateHeartRate(samples: samples)
         }
         return heartRateQuery
     }
     
-    // from https://github.com/coolioxlr/watchOS-2-heartrate/blob/master/VimoHeartRate%20WatchKit%20App%20Extension/InterfaceController.swift
+    // modified from https://github.com/coolioxlr/watchOS-2-heartrate/blob/master/VimoHeartRate%20WatchKit%20App%20Extension/InterfaceController.swift
     func updateHeartRate(samples: [HKSample]?) {
-        guard let heartRateSamples = samples as? [HKQuantitySample] else {return}
+        guard let heartRateSamples = samples as? [HKQuantitySample] else { return }
         
         DispatchQueue.main.async() {
             guard let sample = heartRateSamples.first else { return }
@@ -165,7 +59,6 @@ class InterfaceController: WKInterfaceController, HKWorkoutSessionDelegate {
             self.bpm.setText(String(UInt16(value)))
         }
     }
-    
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
@@ -180,47 +73,33 @@ class InterfaceController: WKInterfaceController, HKWorkoutSessionDelegate {
     
     
     @IBAction func recordIsTapped() {
+        
         let workoutConfiguration = HKWorkoutConfiguration()
         workoutConfiguration.activityType = .other
         workoutConfiguration.locationType = .unknown
         
+        // Inspired by https://developer.apple.com/library/content/samplecode/SpeedySloth/Introduction/Intro.html
         do {
             try self.workoutSession = HKWorkoutSession(configuration: workoutConfiguration)
             healthStore.start(self.workoutSession!)
+            if HKHealthStore.isHealthDataAvailable() {
+                if let query = createHeartRateStreamingQuery() {
+                    self.healthStore.execute(query)
+                }
+            }
+            else {
+                print("Healthkit unavailable")
+            }
         }
         catch {
             fatalError(error.localizedDescription)
         }
         
-        if HKHealthStore.isHealthDataAvailable() {
-            if let query = createHeartRateStreamingQuery() {
-                self.healthStore.execute(query)
-            }
-        }
-        else {
-            print("Healthkit unavailable")
-        }
-        
-        
-        
-//        let heartRate = HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!
-//        let query = HKSampleQuery(sampleType: heartRate, predicate: .none, limit: 0, sortDescriptors: nil) { query, results, error in
-//
-//            guard let count = results?.count else { return }
-//            if count > 0 {
-//                guard let results = results as? [HKQuantitySample] else { return }
-//                for result in results {
-//                    let heartRate = result.quantity.doubleValue(for: HKUnit.count().unitDivided(by: HKUnit.minute()))
-//                    self.bpm.setText(String(heartRate))
-//                }
-//            }
-//        }
-//        self.healthStore.execute(query)
-        
     }
     
     @IBAction func stopIsTapped() {
         guard let sess = self.workoutSession else { return }
+        self.bpm.setText("BPM")
         healthStore.end(sess)
     }
     
